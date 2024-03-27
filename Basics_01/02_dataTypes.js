5598ab1b91482187f1e95b79a972f9bdae5e97ec
Ritika Singh <147723754+Ritika912@users.noreply.github.com>
2024-03-27 13:42:19 +0000

    datatype

---
+++ Basics_01/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version
+//alert("hello") //we are using node js not browser
+//code readability should be high
+
+//number=> 2 ^to 53
+//bigint
+//string=>""
+//boolean=> true / false
+//null=> standalone value
+//undefined=>
+//symbol=> uniqueness
+
+//object
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined

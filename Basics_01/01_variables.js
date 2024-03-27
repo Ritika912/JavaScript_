f039a29746204c55a48a5430a5e4fe6d661aa3ca
Ritika Singh <147723754+Ritika912@users.noreply.github.com>
2024-03-27 13:42:29 +0000

    variables

---
+++ Basics_01/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=13456
+let accountEmail="ritika@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+/*
+Prefer not to use var
+because of issue in block scope and functional scope*/
+
+console.table([accountId ,accountEmail,accountPassword,accountCity]);
